0735702e72a4126d91670f6d358b3b6b05c6265d
alecng27 <alec.nguyen27@gmail.com>
2022-08-07 15:52:59 +0700

    update

--- app.R
+++ app.R
@@ -14,12 +14,17 @@ library(shinyWidgets)
 library(plotly)
 library(kableExtra)
 library(tibble)
+library(sodium)
 options(tigris_use_cache = TRUE)
 ## turn off scientific notation
 options(scipen = 999)
 
-State_Data <- read_rds("Main_Data/State_Data.rds") %>% sf::st_as_sf()
-CoC_Data <- read_rds("Main_Data/CoC_Data.rds") %>% sf::st_as_sf()
+metadata <- readRDS("Main_Data/metadata.rds")
+State_Data <- readRDS("Main_Data/app_system_data_007s.rds")
+CoC_Data <- readRDS("Main_Data/app_system_data_005c.rds")
+
+State_Data <- unserialize(data_decrypt(State_Data, metadata)) %>% sf::st_as_sf()
+CoC_Data <- unserialize(data_decrypt(CoC_Data, metadata)) %>% sf::st_as_sf()
 
 state_data_for_download <- State_Data %>% st_drop_geometry()
 coc_data_for_download <- CoC_Data %>% st_drop_geometry()
